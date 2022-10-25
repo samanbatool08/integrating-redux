8ee5e0a2fcbc057c1182b78dac7861d559eea111
Saman Batool <samanbatool08@gmail.com>
2022-10-25 09:51:15 -0400

    scaffold app component

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      App Comp
+    </div>
+  )
+};
+
+export default App; 
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import react from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+
+ReactDOM.render( <App />,
+                document.querySelector('#root'));
