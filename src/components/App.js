ee7e2267d9fb095b082189b4b61f4d646c2ccdfc
Saman Batool <samanbatool08@gmail.com>
2022-10-25 11:20:11 -0400

    add react-redux dependencies

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { selectSong } from '../actions';
+import SongList from './SongList';
 
 const App = () => {
   return (
     <div>
-      App Comp
+      <SongList />
     </div>
   )
 };
